7a2966bb04775e1abd10e2e4585466b61f07ac06
Jussi <K8270@student.jamk.fi>
2017-01-19 14:58:43 +0200

    teht07 kommentoitu

--- teht07/Program.cs
+++ teht07/Program.cs
@@ -15,7 +15,7 @@ namespace teht07
             Console.Write("Annappa vuosiluku niin kerron onko se karkausvuosi >");
             string syote = Console.ReadLine();
             vuosi = int.Parse(syote);
-
+            //    4 jaollinen         mutta ei sadalla     tai 400lla
             if ((vuosi % 4) == 0 && vuosi % 100 != 0 || vuosi % 400 == 0) {
                 Console.WriteLine("JEP on karkausvuosi");
             }
