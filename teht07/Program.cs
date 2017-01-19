b6e7424fc129f3b84db228f3397e6e2dbc3a7beb
Jussi <K8270@student.jamk.fi>
2017-01-19 14:57:30 +0200

    teht07 valmis

---
+++ teht07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi;
+            
+            Console.Write("Annappa vuosiluku niin kerron onko se karkausvuosi >");
+            string syote = Console.ReadLine();
+            vuosi = int.Parse(syote);
+
+            if ((vuosi % 4) == 0 && vuosi % 100 != 0 || vuosi % 400 == 0) {
+                Console.WriteLine("JEP on karkausvuosi");
+            }
+            else
+            {
+                Console.WriteLine("NOUP");
+            }
+        }
+    }
+}
