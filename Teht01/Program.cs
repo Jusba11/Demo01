f9b917c1d929bfe99960f6a924e75a46859cb525
K8270 <K8270@OHJE-330-WS-17>
2017-01-19 13:36:13 +0200

    Add project files.

---
+++ Teht01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask a number
+            int number;
+            Console.Write("Give a number!! >");
+            string line = Console.ReadLine(); // "1"
+            number = int.Parse(line); // "1" --> 1
+            
+            //which number?
+
+            switch(number)
+            {
+                case 1: Console.WriteLine("yksi"); break;
+                case 2: Console.WriteLine("Kaksi"); break;
+                case 3: Console.WriteLine("Kolme"); break;
+                default: Console.WriteLine("Joku muu luku!"); break;
+            }
+
+            Console.ReadLine(); // tämä sitä varten että debuggerin saa käyttöön jolloin se odottaa vielä yhtä entteriä.
+        }
+    }
+}
---
+++ Teht01b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask a number
+            int number;
+            Console.Write("Give a number!! >");
+            string line = Console.ReadLine(); // "1"
+            bool result = int.TryParse(line,out number); // "1" --> 1
+            if (result == true)
+            {
+
+                switch (number)
+                {
+                    case 1: Console.WriteLine("yksi"); break;
+                    case 2: Console.WriteLine("Kaksi"); break;
+                    case 3: Console.WriteLine("Kolme"); break;
+                    default: Console.WriteLine("Joku muu luku!"); break;
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("JOO EI MENNY PUTKEE.");
+            }
+
+            Console.ReadLine(); // tämä sitä varten että debuggerin saa käyttöön jolloin se odottaa vielä yhtä entteriä.
+        }
+    }
+}
