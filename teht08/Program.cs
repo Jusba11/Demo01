f4af3211e5bddc00b836a8269605769051681cf7
Jussi <K8270@student.jamk.fi>
2017-01-19 15:13:40 +0200

    teht8 alussa

---
+++ teht08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3;
+
+            Console.Write("Anna 1. luku");
+            string syote = Console.ReadLine();
+            luku1 = int.Parse(syote);
+
+            Console.Write("Anna 2. luku");
+            syote = Console.ReadLine();
+            luku2 = int.Parse(syote);
+
+            Console.Write("Anna 3. luku");
+            syote = Console.ReadLine();
+            luku3 = int.Parse(syote);
+            
+        }
+    }
+}
