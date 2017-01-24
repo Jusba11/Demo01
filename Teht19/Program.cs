ad0c29507528db7e4507367a4ab98408cd4c465b
Jussi <K8270@student.jamk.fi>
2017-01-24 23:43:35 +0200

    asd

---
+++ Teht19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arvaukset, hp;
+            char[] sana = new char[7];
+
+        }
+    }
+}
