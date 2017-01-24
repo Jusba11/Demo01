cc40d0ebdfbe9a0046eee53a5af10c2913e075e5
Jussi <K8270@student.jamk.fi>
2017-01-24 15:42:00 +0200

    Teht10 valmis

---
+++ Teht10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = {1, 2, 33, 44, 55, 68, 77, 96, 100};
+            int i;
+
+            for (i = 0; i < luvut.Length; i++)
+            {
+                if ( luvut[i] % 2 == 0)
+                {
+                    Console.Write(luvut[i]);
+                    Console.WriteLine(" HEP!");
+                }
+                
+            }
+
+        }
+        
+    }
+}
