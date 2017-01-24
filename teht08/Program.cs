31cab83dcb49b3b6652266d6763378b44a6b1d82
Jussi <K8270@student.jamk.fi>
2017-01-24 15:26:57 +0200

    teht08 valmis

--- teht08/Program.cs
+++ teht08/Program.cs
@@ -10,21 +10,36 @@ namespace teht08
     {
         static void Main(string[] args)
         {
-            int luku1, luku2, luku3;
 
-            Console.Write("Anna 1. luku");
+            int luku1, luku2, luku3, suurin;
+
+            Console.Write("Anna 1. luku >");
             string syote = Console.ReadLine();
             luku1 = int.Parse(syote);
 
-            Console.Write("Anna 2. luku");
+            Console.Write("Anna 2. luku >");
             syote = Console.ReadLine();
             luku2 = int.Parse(syote);
 
-            Console.Write("Anna 3. luku");
+            Console.Write("Anna 3. luku >");
             syote = Console.ReadLine();
             luku3 = int.Parse(syote);
-            
-            //joo
+
+            suurin = luku1; // alkuun laitetaan eka luku muka suurimmaksi jotta voidaan lähteä vertaamaan
+
+            if (luku2 > suurin)
+            {
+                suurin = luku2;
+            }
+
+            if (luku3 > suurin)
+            {
+                suurin = luku3;
+            }
+
+            Console.Write("Suurin luvuista oli: ");
+            Console.WriteLine(suurin);
+
         }
     }
 }
