dd0fddb0e5be6f061d0737ebb8bc99852fbb71f4
Jussi <K8270@student.jamk.fi>
2017-01-24 19:08:34 +0200

    Teht15 valmis

--- teht15/Program.cs
+++ teht15/Program.cs
@@ -10,7 +10,7 @@ namespace teht15
     {
         static void Main(string[] args)
         {
-            int luku, i, leveys, kerrokset = 1 ;
+            int luku, i, j, k, leveys, safeLeveys, kerrokset = 1, valilyonnit ;
 
             Console.Write("Anna luku >");
             string syote = Console.ReadLine(); // luetaan käyttäjän syöte
@@ -18,9 +18,11 @@ namespace teht15
 
             luku = luku - 3;         //miinustamalla 3 saadaan alimman kerroksen leveys
             leveys = (luku) * 2 + 1; // tästä kaavasta.
-            
-
+            safeLeveys = (luku) * 2 + 1;
+            /*
+            Console.Write("Leveys: ");
             Console.WriteLine(leveys);
+            */
 
             do // lasketaan kerrosten määrä
             {
@@ -29,8 +31,38 @@ namespace teht15
 
             } while (leveys != 1);
 
-            
+            /*
+            Console.Write("Kerrosten maara: ");
             Console.WriteLine(kerrokset);
+            */
+
+            // latvaan tarvittavien välilyöntien määrä = leveys / 2 - 1
+            //seuraaviin tarvittavat välilyönnit = leveys / 2 - 2,3,4...
+            
+            for (i = 0; i < kerrokset; i++)
+            {
+                for (j = kerrokset; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (k = 0; k < i * 2 + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                
+                Console.WriteLine();
+            } // tässä vaiheessa kuusi on piirretty, enää jalka puuttuu..
+            
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < safeLeveys / 2 + 1; j++)
+                {      /*           ^  Alkuperäinen leveys johon ei ole koskettu*/
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+
 
         }
     }
