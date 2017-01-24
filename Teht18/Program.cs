3dfc42750612c38cefa53614d99b15aee7b57cae
Jussi <K8270@student.jamk.fi>
2017-01-24 23:36:50 +0200

    Teht18 Valmis

--- Teht18/Program.cs
+++ Teht18/Program.cs
@@ -11,17 +11,24 @@ namespace Teht18
         static void Main(string[] args)
         {
             int i, j, k;
-            string syote2;
 
             Console.Write("Kirjoita jotain niin kerron onko kirjoittamasi asia palidromi>");
             string syote = Console.ReadLine(); // luetaan käyttäjän syöte
 
             char[] syote_reverse = syote.ToCharArray();
             Array.Reverse(syote_reverse);
-            new string(syote_reverse);
+            string syote2 = new string(syote_reverse);
 
-
-
+            if (syote == syote2)
+                {
+                    Console.WriteLine("JOOO! Palidromi!");
+                }
+                else
+                {
+                    Console.WriteLine("NO EI OLE.");
+            
+                }
         }
+        
     }
 }
