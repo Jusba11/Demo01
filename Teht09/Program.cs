ae73329e81ea0819790efcf92fcd64a80469fa63
Jussi <K8270@student.jamk.fi>
2017-01-24 15:33:34 +0200

    teht09 valmis

---
+++ Teht09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0, luku;
+
+            do
+            {
+                
+                Console.Write("Anna luku, jos syotat 0 niin ohjelma loppuu >");
+                string syote = Console.ReadLine(); // luetaan käyttäjän syöte
+                luku = int.Parse(syote); //parsitaan syöttestä luku
+                summa = (summa + luku);
+
+            } while (luku != 0);
+
+            Console.Write("Antamiesi lukujen summa on: ");
+            Console.WriteLine(summa);
+        }
+    }
+}
